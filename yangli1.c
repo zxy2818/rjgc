2a85051b9be37df532ed77e1320d8418e6b45ebc
zxy~ <60543763+zxy2818@users.noreply.github.com>
2020-02-11 13:43:44 +0800

    Update yangli1.c

--- yangli1.c
+++ yangli1.c
@@ -8,6 +8,7 @@ int num(char d[]){
         return hzhuans(d); 
     }
 }
+
 int hzhuans(char d[]){
     int i;
     for(i=0;i<11;i++){
@@ -16,17 +17,21 @@ int hzhuans(char d[]){
         }
     }
 }
+
 int jiafa(int x,char c[]){
 
     int y;
     y=num(c);
-    return x+y;
+	return x+y;
+  
 }
+
 int jianfa(int x,char c[]){
 
     int y;
-    y=num(c);
-    return x-y;
+	y=num(c);
+	return x-y;
+    
 }
 
 
@@ -35,33 +40,35 @@ int main(){
     char a[20],b[20],c[20],d[20],e[20],f[20],g[20],h[20],i[20],j[20];
 
     printf("请输入:\n");
-	  printf("\n");
+	printf("\n");
 
     scanf("%s %s %s %s",a,b,c,d);//整数 气温 等于 十
     sum=num(d);
 
     while(scanf("%s %s",a,b)!=EOF){
        if(strcmp(a,"看看")){
-		       scanf("%s",c); 
-		       if(strcmp(b,"增加")==0){
-			           sum=jiafa(sum,c);
-		       }
+		   scanf("%s",c); 
+		   if(strcmp(b,"增加")==0){
+			   sum=jiafa(sum,c);
+		   }
            if(strcmp(b,"减少")==0){
-                sum=jianfa(sum,c); 
-		       }
-	     }
+               sum=jianfa(sum,c); 
+		   }
+	   }
        else{
-		        scanf("%s %s %s %s %s %s %s %s %s %s",a,b,c,d,e,f,g,h,i,j);
+		    scanf("%s %s %s %s %s %s %s %s %s %s",a,b,c,d,e,f,g,h,i,j);
             printf("\n");  
-			      printf("%s\n",hanzi[sum]);
-		        if(sum>8)
-			           printf("你好,世界\n");
-	          else
-			           printf("冻死我了\n");
-			  return 0;
-	     }
+			printf("%s\n",hanzi[sum]);
+		    if(sum>8)
+			    printf("你好,世界\n");
+	        else
+			    printf("冻死我了\n");
+			return 0;
+	   }
 	}
    return 0;       
 }
+ 
+
  
 
